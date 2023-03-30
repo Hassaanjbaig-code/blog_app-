a18563fbbf4f7ed20ef2c04ff1c3b63093b6c66e
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-30 21:02:46 +0500

    fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :user_id
   has_many :posts, foreign_key: :author_id
 
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
