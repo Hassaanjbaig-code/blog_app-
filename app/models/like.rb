2f8c085085eab90ad3604de93799cf2905102354
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-30 20:57:14 +0500

    Fix the linter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    def update_the_like_counter
-        post.update(likescounter: post.likescounter + 1)
-    end
+  def update_the_like_counter
+    post.update(likescounter: post.likescounter + 1)
+  end
 end
