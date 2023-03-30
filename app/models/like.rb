bea4f623c45423e85045abcbe126f066b5dd7b09
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-30 22:29:20 +0500

    Fix the code according to the review

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_create :update_the_like_counter
 
   def update_the_like_counter
     post.update(likescounter: post.likescounter + 1)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,13 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
   belongs_to :author, class_name: 'User'
+  after_create :update_post_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  def update_the_post_counter
+  def update_post_counter
     author.update(postscounter: author.postscounter + 1)
   end
 end
