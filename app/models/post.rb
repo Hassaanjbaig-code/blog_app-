258a5032488042352c5389680653156f4f40592b
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-31 11:56:12 +0500

    created the validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
   belongs_to :author, class_name: 'User'
+    validates :title, presence: true, lenght: { maximum: 250 }
+    validates :commentscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :likescounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_create :update_post_counter
 
   def recent_comments
