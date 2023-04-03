fa8eddda7161cbfdabcb8093613339865cd49c64
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-03 20:16:38 +0500

    users show in working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#index'
+  # get '/', to: 'users#index'
+  root 'users#index'
   resources :users
+  resources :post
 
   # Defines the root path route ("/")
   # root "articles#index"
