a2403846c8961b44793dcf4d4686b3b0ee04da3a
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-04 23:02:12 +0500

    Workng of the user controller

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
+require 'rails-controller-testing'
 
 RSpec.describe UsersController, type: :request do
     describe 'GET /users' do
-        before do
-            @user = User.create(name: 'Raihan', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                                bio: 'Teacher from Bangla.', postscounter: 0)
-        end
-        it 'should return the users' do
-            get users_path
+        before(:example) { get '/users'}
+        it 'should check the responce is comming' do
             expect(response).to have_http_status(:ok)
         end
-        # it 'should return the user' do
-        #     get '/users/1'
-        #     expect(response).to have_http_status(:ok)
-        # end
-        # it 'should return the user posts' do
-        #     get '/users/1/posts'
-        #     expect(response).to have_http_status(:ok)
-        # end
-        # it 'should return the user post' do
-        #     get '/users/1/posts/1'
-        #     expect(response).to have_http_status(:ok)
-        # end
+        it('is correct template rendered') do
+            expect(response).to render_template(:index)
+        end
+        it 'should check the responce is comming' do
+            expect(response.body.size).to eq(6635)
+        end
+    end
+    describe 'GET /users/:id' do
+        before(:example) { get '/users/1'}
+        it 'should check the responce is comming' do
+            expect(response).to have_http_status(:ok)
+        end
+        it('is correct template rendered') do
+            expect(response).to render_template(:show)
+        end
     end
 end
