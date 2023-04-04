d87bb46a40296b7525fe5047fd5c6939f33fdd92
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-04 23:16:06 +0500

    Fix the linter

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 require 'rails-controller-testing'
 
 RSpec.describe PostsController, type: :request do
-    describe 'GET /posts' do
-        before(:example) { get '/users/1/posts' }
-        it 'should check the responce is comming' do
-        expect(response).to have_http_status(:ok)
-        end
-        it('is correct template rendered') do
-        expect(response).to render_template(:index)
-        end
+  describe 'GET /posts' do
+    before(:example) { get '/users/1/posts' }
+    it 'should check the responce is comming' do
+      expect(response).to have_http_status(:ok)
     end
-    describe 'GET /posts/:id' do
-        before(:example) { get '/users/1/posts/1' }
-        it 'should check the responce is comming' do
-        expect(response).to have_http_status(:ok)
-        end
-        it('is correct template rendered') do
-        expect(response).to render_template(:show)
-        end
-        it('is response body includes correct placeholder text') do
-            expect(response.body).to include('Show the detail of the post')
-          end
+    it('is correct template rendered') do
+      expect(response).to render_template(:index)
     end
+  end
+  describe 'GET /posts/:id' do
+    before(:example) { get '/users/1/posts/1' }
+    it 'should check the responce is comming' do
+      expect(response).to have_http_status(:ok)
+    end
+    it('is correct template rendered') do
+      expect(response).to render_template(:show)
+    end
+    it('is response body includes correct placeholder text') do
+      expect(response.body).to include('Show the detail of the post')
+    end
+  end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 require 'rails-controller-testing'
 
 RSpec.describe UsersController, type: :request do
-    describe 'GET /users' do
-        before(:example) { get '/users'}
-        it 'should check the responce is comming' do
-            expect(response).to have_http_status(:ok)
-        end
-        it('is correct template rendered') do
-            expect(response).to render_template(:index)
-        end
-        it 'should check the responce is comming' do
-            expect(response.body.size).to eq(6635)
-        end
+  describe 'GET /users' do
+    before(:example) { get '/users' }
+    it 'should check the responce is comming' do
+      expect(response).to have_http_status(:ok)
     end
-    describe 'GET /users/:id' do
-        before(:example) { get '/users/1'}
-        it 'should check the responce is comming' do
-            expect(response).to have_http_status(:ok)
-        end
-        it('is correct template rendered') do
-            expect(response).to render_template(:show)
-        end
+    it('is correct template rendered') do
+      expect(response).to render_template(:index)
     end
+    it 'should check the responce is comming' do
+      expect(response.body.size).to eq(6635)
+    end
+  end
+  describe 'GET /users/:id' do
+    before(:example) { get '/users/1' }
+    it 'should check the responce is comming' do
+      expect(response).to have_http_status(:ok)
+    end
+    it('is correct template rendered') do
+      expect(response).to render_template(:show)
+    end
+  end
 end
