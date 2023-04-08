49e82a6535492671fc30665407e52288fdc228c7
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-08 14:41:54 +0500

    Add a style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
   def new
     @user = current_user(params[:user_id])
     @post = Post.find(params[:post_id])
-    @comment = Comment.new
+    @comment = Comment.create
   end
 
   def create
     @user = current_user(params[:user_id])
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(
+    @comment = Comment.create(
       author: @user,
       post: @post,
       text: params[:comment][:text]
