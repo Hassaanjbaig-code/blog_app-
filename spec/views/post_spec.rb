106cfab20a0ea1fbd8c628f2f0dcc13967cc5ad8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-11 20:19:54 +0500

    All the test for the post is working

--- spec/views/post_spec.rb
+++ spec/views/post_spec.rb
@@ -2,40 +2,40 @@ require 'rails_helper'
 require 'rails-controller-testing'
 
 RSpec.describe Post, type: :system do
-#   describe 'index' do
-#     before do
-#       @user = User.create(name: 'Raihan', photo: "https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxM[%E2%80%A6]8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
-#         bio: 'Teacher from Bangla.', postscounter: 1,
-#         created_at: '2023-03-30 12:47:48.768257', updated_at: '2023-03-30 12:47:48.768257')
-#       @post = Post.create(author: @user, title: 'Hello World', text: 'This is my first post',
-#         commentscounter: 0, likescounter: 0)
-#         visit user_posts_path(@user, @post)
-#     end
+  describe 'index' do
+    before do
+      @user = User.create(name: 'Raihan', photo: "https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxM[%E2%80%A6]8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
+        bio: 'Teacher from Bangla.', postscounter: 1,
+        created_at: '2023-03-30 12:47:48.768257', updated_at: '2023-03-30 12:47:48.768257')
+      @post = Post.create(author: @user, title: 'Hello World', text: 'This is my first post',
+        commentscounter: 0, likescounter: 0)
+        visit user_posts_path(@user, @post)
+    end
 
-#     it 'shows the right content test the name is present' do
-#         expect(page).to have_content(@user.name)
-#     end
-#     it 'shows the right content test the post is present' do
-#         expect(page).to have_content(@post.text)
-#     end
-#     it "show the image of the user" do
-#         expect(page.find('img')[:src]).to eq(@user.photo)
-#     end
-#     it "should have a Comment " do 
-#         expect(page).to have_content('Comment')
-#     end
-#     it 'I can see the number of comments' do
-#         expect(page).to have_content('omments: 0')
-#       end
-#       it 'I can see the number of likes' do
-#         expect(page).to have_content('likes: 0')
-#       end
-#     it "When I click on the post I see the detail post" do
-#         click_link 'This is my first post'
-#         sleep 1
-#         expect(page).to have_content("Post #{@post.id} by #{@user.name}")
-#     end
-#   end
+    it 'shows the right content test the name is present' do
+        expect(page).to have_content(@user.name)
+    end
+    it 'shows the right content test the post is present' do
+        expect(page).to have_content(@post.text)
+    end
+    it "show the image of the user" do
+        expect(page.find('img')[:src]).to eq(@user.photo)
+    end
+    it "should have a Comment " do 
+        expect(page).to have_content('Comment')
+    end
+    it 'I can see the number of comments' do
+        expect(page).to have_content('omments: 0')
+      end
+      it 'I can see the number of likes' do
+        expect(page).to have_content('likes: 0')
+      end
+    it "When I click on the post I see the detail post" do
+        click_link 'This is my first post'
+        sleep 1
+        expect(page).to have_content("Post #{@post.id} by #{@user.name}")
+    end
+  end
   describe 'show' do
     before do
         @user = User.create(name: 'Raihan', photo: "https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxM[%E2%80%A6]8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
